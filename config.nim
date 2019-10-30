30b27ecfae57100d0dca1c501e8f2cd90b94f0c2
me7 <gampolt@gmail.com>
2019-10-30 11:56:08 +0700

    bump version

--- config.nim
+++ config.nim
@@ -1,5 +1,5 @@
 const
     pkgTitle*       = "srv"
-    pkgVersion*     = "0.2.0"
+    pkgVersion*     = "0.3.0"
     pkgAuthor*      = "Gampol T. (fork from Fabio Cevasco)"
     pkgDescription* = "Static file web server."
